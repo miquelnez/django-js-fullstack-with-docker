adccce82cdbfb7a914740154bca51f10c6fd1925
velodev <dev@velodev.co>
2019-09-08 21:18:42 +0200

    postgres working

---
+++ app/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# python manage.py flush --no-input # flush data with every start
+python manage.py migrate
+
+exec "$@"
